ef84c58e1fe2c070ad8726c490cbc4ee54fe2c2c
Andrea <andrea@andreas-mini.home>
2017-10-29 21:01:56 +0000

    Added sample data and build first gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
-gulpfile.js
+'use strict';
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+ 
+gulp.task('scripts', function () {
+	gulp.src('src/**/*.js')
+	.pipe(uglify())
+	.pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass', function() {
+    gulp.src('src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'))
+});
+
+
+gulp.task('watch', function(){
+	gulp.watch('src/**/*.js', ['scripts']);
+	gulp.watch('src/**/*.scss', ['scss']);
+});
+
+gulp.task('default', ['sass', 'scripts']);
