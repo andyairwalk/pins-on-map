2b893660c68dfe4f5021c72c339fe68c07ef9caf
Andrea <andrea@andreas-mini.home>
2017-11-12 13:34:33 +0000

    On submit add the pin on the map(front end side)

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,54 @@
 // Center the map
 var map = new google.maps.Map(document.getElementById('map'), {
-	zoom: 1,
-	center: new google.maps.LatLng(-33.92, 151.25),
+	zoom: 7,
+	center: new google.maps.LatLng(0, 0),
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 });
 
 var infowindow = new google.maps.InfoWindow();
-
 var marker, i;
 
-// test add pin
-$("body").on("click", "#test-add-button", function(){
-	marker = new google.maps.Marker({
-		position: new google.maps.LatLng(0, 0),
-		map: map
-	});
+// add pins on map via json
+$.ajax({
+	url: "https://api.mongolab.com/api/1/databases/locations/collections/pins?apiKey=LgEjQO7xxGnnfoqMELFaVQ1TuPuVkDw7"
+}).done(function(data){
+	console.log(data);
+	var total_pins = Object.keys(data).length;
 
-	google.maps.event.addListener(marker, 'click', (function(marker) {
-		return function() {
-			infowindow.setContent("test something");
-			infowindow.open(map, marker);
-		}
-	})(marker));
+	for (i = 0; i < total_pins; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(data[i].lat, data[i].lng),
+			map: map
+		});
+
+		google.maps.event.addListener(marker, 'click', (function(marker, i) {
+			return function() {
+				infowindow.setContent(data[i].name);
+				infowindow.open(map, marker);
+			}
+		})(marker, i));
+	}
 });
- 
-// on form submit add the pin on the map
+
+// on form submit add the pin on the map and on the json file
 $("body").on("submit", "#form", function(e){
 	e.preventDefault();
 
 	var name = $("#name").val();
 	var lat = $("#lat").val();
 	var lng = $("#lng").val();
+
+	marker = new google.maps.Marker({
+		position: new google.maps.LatLng(lat, lng),
+		map: map
+	});
+
+	google.maps.event.addListener(marker, 'click', (function(marker) {
+		return function() {
+			infowindow.setContent(name);
+			infowindow.open(map, marker);
+		}
+	})(marker));
 
 	$.ajax({
 		url: "https://api.mongolab.com/api/1/databases/locations/collections/pins?apiKey=LgEjQO7xxGnnfoqMELFaVQ1TuPuVkDw7",
@@ -54,56 +72,3 @@ $("body").on("submit", "#form", function(e){
 		}
 	})
 });
-
-// add pins on map via json
-$.ajax({
-	url: "https://api.mongolab.com/api/1/databases/locations/collections/pins?apiKey=LgEjQO7xxGnnfoqMELFaVQ1TuPuVkDw7"
-}).done(function(data){
-	console.log(data);
-	var total_pins = Object.keys(data).length;
-
-	for (i = 0; i < total_pins; i++) {
-	console.log();  
-		marker = new google.maps.Marker({
-			position: new google.maps.LatLng(data[i].lat, data[i].lng),
-			map: map
-		});
-
-		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-			return function() {
-				infowindow.setContent(data[i].name);
-				infowindow.open(map, marker);
-			}
-		})(marker, i));
-	}
-});
-
-
-// var request = new XMLHttpRequest();
-// request.open("GET", "dist/db/locations.json", false);
-// request.send(null);
-// request.onreadystatechange = function() {
-//   if ( request.readyState === 4 && request.status === 200 ) {
-//     var my_JSON_object = JSON.parse(request.responseText);
-//     console.log(my_JSON_object);
-//   }
-// }
-
-/////
-// getArray().done( function(json) {
-// 	console.log("done");
-//     console.log(json); // show the json data in console
-//     var _len = json.length;
-//     var fixture;
-
-
-//     //loop through json and match today's date with match-date
-//     // for (var i in json) {
-//     //     fixture = json[i];
-//     //     if (fixture.date == today) {
-//     //         //print out today's schedule here
-//     //         console.log(fixture.team1 + " Vs. " + fixture.team2);
-//     //     }
-//     // }
-
-// });
